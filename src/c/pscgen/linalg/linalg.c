73e3d7ae94f19052bd5b14fefab1a44b3e9624b8
Brad McDanel <mcdabw6@gmail.com>
2015-10-24 13:45:12 -0400

    forgot linalg.c

---
+++ src/c/pscgen/linalg/linalg.c
@@ -0,0 +1,96 @@
+#include "linalg.h"
+
+double* dmv_prod(double *A, double *x, int rows, int cols)
+{
+    double* y = malloc(sizeof(double) * cols);
+
+    //TODO: confirm initalization is needed
+    int i;
+    for(i = 0; i < cols; i++) {
+        y[i] = 0.0;
+    }
+
+    char no = 'N', tr = 'T';
+    int lda = cols, incx = 1, incy = 1;
+    double alpha = 1.0, beta = 0.0;
+    printf("%d,%d\n", rows, cols);
+
+    dgemv_(&no, &rows, &cols, &alpha, A, &lda, x, &incx, &beta, y, &incy);
+
+    return y;
+}
+
+//A = M * K, B = K * N
+double* dmm_prod(double *A, double *B, int A_rows, int A_cols, int B_rows,
+                 int B_cols)
+{
+    if(A_cols != B_rows) {
+        printf("dgemm matrix mismatch:\nA:%d, %d\nB:%d, %d\n", A_rows, A_cols,
+               B_rows, B_cols);
+        exit(1);
+    }
+
+    double* C = malloc(sizeof(double) * A_rows * B_cols);
+
+    int i, j;
+    for(i = 0; i < A_rows; i++) {
+        for(j = 0; j < B_cols; j++) {
+            /* printf("%d,%d\n", i, j); */
+            C[idx2d(i, j, B_cols)] = 0.0;
+        }
+    }
+
+    char no = 'N';
+    double alpha = 1.0, beta = 0.0;
+
+    dgemm_(&no, &no, &A_rows, &B_cols, &A_cols, &alpha, A, &A_rows, B, &B_rows,
+           &beta, C, &A_rows);
+           
+    return C;
+}
+
+
+/* Returns the eigenvectors of a matrix A */
+double* deig_vec(double *input, int rows, int cols) {
+    int info, lwork;
+    double wkopt;
+    double *work;
+
+    //TODO: switch to memcpy
+    double *A = malloc(sizeof(double) * rows * cols);
+    int i;
+    for(i = 0; i < rows*cols; i++)
+        A[i] = input[i];
+
+    double *S = malloc(sizeof(double) * MIN(rows, cols));
+    double *U = malloc(sizeof(double) * cols * cols);
+    double *VT = malloc(sizeof(double) * rows * rows);
+
+    /* Query and allocate the optimal workspace */
+    lwork = -1;
+    dgesvd_("All", "All", &cols, &rows, A, &cols, S, U, &cols, VT,
+            &rows, &wkopt, &lwork, &info);
+    lwork = (int)wkopt;
+    work = malloc(sizeof(double) * lwork);
+
+    /* Compute SVD */
+    dgesvd_("All", "All", &cols, &rows, A, &cols, S, U, &cols, VT,
+            &rows, work, &lwork, &info);
+
+    /* Check for convergence */
+    if(info > 0) {
+        printf("The algorithm computing SVD failed to converge.\n");
+        exit(1);
+    }
+
+    //clean-up
+    free(A);
+    free(work);
+    free(S);
+    free(U);
+
+    //transpose vt to v
+    /* d_transpose_inplace(VT, rows, rows); */
+
+    return VT;
+}
